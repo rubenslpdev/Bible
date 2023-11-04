0a846cddca634de09123374c444b8b4d8feeb625
rubenslpdev <rubenslp@gmail.com>
2023-11-04 07:46:13 -0300

    initialize project

---
+++ LEIAME.md
@@ -0,0 +1,39 @@
+# Bíblia Sagrada online
+
+## Objetivo
+
+Criar um site / aplicativo com os textos do antigo e do novo Testamento para todas plataformas, sem nenhum tipo de publicidade e que traga a melhor experiência de leitura para o usuário.
+
+## Público
+
+O aplicativo será público e aberto para todas as idades, classes, gêneros, etc, porém a direção artítstica têm como público alvo jovens entre 15-25 anos. O aplicativo deve inspirar a vontade da leitura e facilitar ao máximo o acesso ao público que sofre constantemente o assédio de marcas em redes sociais pela sua atenção. Para este público, pode se tornar enfadonha a leitura da Bíblia quando sua atenção é disputada por agentes economicamente fortes e com estratégias muito mais apelativas.
+
+Cooptar a atenção do público jovem para os textos sagrados é essencial para o desenvolvimento saudável da sua personalidade em formação e consequentemente para o bem-estar da sociedade como um todo.
+
+## Desenvolvimento
+
+O aplicativo será desenvolvido com o uso de HTML, CSS, Javascript e React. É usada a API ... para baixar o banco de dados dos textos. A API é gratuita e aberta para uso.
+
+Sobre o uso de Cookies
+
+O projeto teve início em 27/09/2023 e foi publicado em ...
+
+## Recursos
+
+Acessibilidade deve ser o ponto central do aplicativo. A navegação deve ser fácil e fluída e toda configuração de acessibilidade deve estar já na tela ou a 1 clique de distância.
+Opções que podem ser implementadas:
+
+- Dark mode
+- Tamanho da fonte
+- Fonte serifada e sem serifa
+- Favoritar
+- Marcar texto (ou sublinhar)
+- Campo de busca
+- Menu principal
+- Ouvir
+- Continuar de onde parou
+- Mostrar a página e capítulo atual
+
+## Idéias
+
+- Usar AI para gerar algumas imagens bíblicas para ilustrar HOME e algumas outras áreas(?)
---
+++ script.js
@@ -0,0 +1,86 @@
+// ======
+// MENU
+// ======
+
+// SIDE NAVIGATION
+const mySidevav = document.getElementById("mySidenav");
+const sidenavOverlay = document.querySelector(".overlay");
+const closeBtn = document.querySelector(".closebtn");
+const menuBtn = document.querySelector(".menu-btn");
+
+menuBtn.addEventListener("click", function () {
+  mySidevav.style.width = "250px";
+  sidenavOverlay.classList.add("open-overlay");
+});
+
+closeBtn.addEventListener("click", function () {
+  mySidevav.style.width = "0";
+  sidenavOverlay.classList.remove("open-overlay");
+});
+
+sidenavOverlay.addEventListener("click", function () {
+  mySidevav.style.width = "0";
+  sidenavOverlay.classList.remove("open-overlay");
+});
+
+mySidevav.addEventListener("click", function (e) {
+  e.stopPropagation();
+});
+
+// SEARCH NAVIGATION
+const mySearchnav = document.getElementById("mySearchnav");
+const searchBtn = document.querySelector(".fa-rectangle-list");
+const closeSearchBtn = document.querySelector(".search-closebtn");
+
+searchBtn.addEventListener("click", function () {
+  const isMobile = window.matchMedia("(max-width: 500px)").matches;
+
+  if (isMobile) {
+    mySearchnav.style.width = "100%";
+  } else {
+    mySearchnav.style.width = "450px";
+  }
+
+  // document.body.style.overflowY = "hidden";
+  sidenavOverlay.classList.add("open-overlay");
+});
+
+closeSearchBtn.addEventListener("click", function () {
+  mySearchnav.style.width = "0";
+  // setTimeout(function () {
+  //   document.body.style.overflowY = "auto";
+  // }, 500);
+  sidenavOverlay.classList.remove("open-overlay");
+});
+
+sidenavOverlay.addEventListener("click", function () {
+  mySearchnav.style.width = "0";
+  sidenavOverlay.classList.remove("open-overlay");
+});
+
+mySearchnav.addEventListener("click", function (e) {
+  e.stopPropagation();
+});
+
+// Dark MODE
+
+const btnDarkMode = document.getElementById("check");
+const theme = window.localStorage.getItem("theme");
+// const prefersDarkTheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+if (theme === "dark") {
+  document.body.classList.add("dark");
+  btnDarkMode.checked = true;
+}
+
+btnDarkMode.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  if (theme === "dark") {
+    window.localStorage.setItem("theme", "light");
+  } else window.localStorage.setItem("theme", "dark");
+});
+
+// if (prefersDarkTheme.matches) {
+//   document.body.classList.add("dark");
+//   btnDarkMode.checked = true;
+// }
